58274f9c5b35bc62d863044fbe495bb31c63f2e4
david <ddb048@gmail.com>
2022-08-11 16:01:21 -0700

    phase 6

--- phase-5.sql
+++ phase-5.sql
@@ -1,14 +1,23 @@
 -- Find the first owner that has a last name containing the lowercase letter "r"
--- Your code here
+-- SELECT * FROM owners WHERE last_name LIKE '%r%' LIMIT 1;
 
--- Find the name and birth year of all the cats ordered by descending birth year
--- Your code here
+-- -- Find the name and birth year of all the cats ordered by descending birth year
+-- SELECT name, birth_year FROM cats ORDER BY birth_year DESC;
 
--- Find the name of the cats who have an owner whose first name begins with an "H"
--- Your code here
+-- -- Find the name of the cats who have an owner whose first name begins with an "H"
+-- SELECT cats.name FROM cats
+--     JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+--     JOIN owners ON(owners.id = cat_owners.owner_id)
+--     WHERE owners.first_name LIKE 'H%';
+
 
 -- Find the first and last names of the owner whose cats are born after the year 2015
--- Your code here
-
+-- SELECT owners.first_name, owners.last_name FROM cats
+--     JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+--     JOIN owners ON(cat_owners.owner_id = owners.id)
+--     WHERE cats.birth_year > 2015;
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
--- Your code here
+SELECT DISTINCT cats.name FROM owners
+    JOIN cat_owners ON (owners.id = cat_owners.owner_id)
+    JOIN cats ON(cat_owners.cat_id = cats.id)
+    WHERE owners.first_name IN ('George', 'Melynda') AND owners.last_name IN ('Beatty', 'Abshire');

51756e4f76184aa8b4fde2d26268e17038f62241
nachen98 <nachen98@gmail.com>
2022-08-11 14:24:52 -0700

    phase3

--- phase-3.sql
+++ phase-3.sql
@@ -1,5 +1,14 @@
 -- Find Hermione's cats
 -- Your code here
+SELECT cats.name FROM cats
+    JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+    JOIN owners ON(owners.id = cat_owners.owner_id)
+    WHERE owners.first_name = 'Hermione';
 
 -- Find All the Toys for Hermione's cats
 -- Your code here
+SELECT toys.name FROM toys
+    JOIN cats ON (toys.cat_id = cats.id)
+    JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+    JOIN owners ON(owners.id = cat_owners.owner_id)
+    WHERE owners.first_name = 'Hermione'

3b6c338128c333d914a863031931db48aea5251e
david <ddb048@gmail.com>
2022-08-11 13:23:44 -0700

    schema'

---
+++ phase-1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cats (
+    name VARCHAR(40) NOT NULL,
+    birth_year INTEGER NOT NULL
+);
+
+CREATE TABLE owners (
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE toys (
+    name VARCHAR(40) NOT NULL
+);
